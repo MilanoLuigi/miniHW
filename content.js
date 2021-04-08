682be14b8afc893bb91464fe9ae88ec3952299f4
Luigi <dogmaaltair@gmail.com>
2021-04-08 16:15:31 +0200

    content js

---
+++ content.js
@@ -0,0 +1,47 @@
+const contenuti = [
+  {
+    titolo: 'Pistola Arcade',
+    immagine: 'src/hello-i-m-nik-BIqTcb-52yg-unsplash.jpg',
+    descrizione: 'Pistola arcade utilizzata nelle vecchie console a partire dagli anni '80'
+  },
+  {
+    titolo: 'Joypad Xbox',
+    immagine: 'src/kote-puerto-fNAJ3waDcDI-unsplash.jpg',
+    descrizione: 'Primo joypad presentato da Microsoft per la sua primissima console'
+  },
+  {
+    titolo: 'Pad Ps4',
+    immagine: 'src/federico-vitale-7QvJbALhefo-unsplash.jpg',
+    descrizione: 'Pad presentato da Sony per la console Ps4'
+  },
+  {
+    titolo: 'Smartphone',
+    immagine: 'src/onur-binay-cQ90QkreiPQ-unsplash.jpg',
+    descrizione: 'Grazie ai mobile games lo smartphone diventa un controller'
+  },
+  {
+    titolo: 'Joy-con',
+    immagine: 'src/enrique-vidal-flores-zsXvRKtsf6Q-unsplash.jpg',
+    descrizione: 'Controller creati da Nintendo per la sua console Nintendo Switch'
+  },
+  {
+    titolo: 'Pad Xbox One',
+    immagine: 'src/diego-marin-dMoBlbpDnAE-unsplash.jpg',
+    descrizione: 'Pad di casa Microsoft prodotto per la nuova next-gen di console Xbox e Pc'
+  },
+  {
+    titolo: 'Pad ps5',
+    immagine: 'src/onur-binay-o-jLc7hSJm0-unsplash.jpg',
+    descrizione: 'Pad di casa Sony con aggiunta dei trigger sensitivi per la nuova console ps5'
+  },
+  {
+    titolo: 'Realtà Virtuale',
+    immagine: 'src/Vr.jpg',
+    descrizione: 'Visore per la realtà virtuale'
+  },
+  {
+    titolo: 'Mouse & tastiera',
+    immagine: 'src/E-sport.jpg',
+    descrizione: 'Pc master race tutta la vita, abbasso le console'
+  }
+]
